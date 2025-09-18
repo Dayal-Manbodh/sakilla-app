f8ca241e1513ac3bcb61a53b4323bebebbf95aa5
Dayal-Manbodh <151162763+Dayal-Manbodh@users.noreply.github.com>
2025-09-18 19:33:15 +0200

    online database

--- app.js
+++ app.js
@@ -29,18 +29,19 @@ app.use("/customers", customersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  next(createError(404));
+  next(createError(404, "Pagina niet gevonden"));
 });
 
 // error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  const status = err.status || 500;
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+  res.status(status);
+  res.render("error", {
+    title: `Fout ${status}`,
+    message: err.message || "Er is een onbekende fout opgetreden",
+    error: req.app.get("env") === "development" ? err : {},
+  });
 });
 
 module.exports = app;
--- db/connection.js
+++ db/connection.js
@@ -1,13 +1,23 @@
 const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
+  host: process.env.DB_HOST, // bv. db-mysql-ams3-46626-do-user-8155278-0.b.db.ondigitalocean.com
+  port: process.env.DB_PORT, // 25060
+  user: process.env.DB_USER, // 2219243
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
+});
+
+pool.getConnection((err, connection) => {
+  if (err) {
+    console.error("❌ Database connection failed:", err.message);
+  } else {
+    console.log("✅ Connected to MySQL database:", process.env.DB_NAME);
+    connection.release(); // verbinding teruggeven aan de pool
+  }
 });
 
 module.exports = pool;
